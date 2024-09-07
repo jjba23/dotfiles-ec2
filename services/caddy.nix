2c1a813291400b4d0e1dff0e000640afee6e727b
Josep Bigorra <jjbigorra@gmail.com>
2024-09-07 17:02:49 +0200

    same

--- services/caddy.nix
+++ services/caddy.nix
@@ -3,21 +3,19 @@
     enable = true;
     globalConfig = ''
       default_bind 0.0.0.0
+      http_port 7979
+      https_port 7979
     '';
     virtualHosts."grafana.jointhefreeworld.org".extraConfig = ''
       basic_auth {
         root $2a$14$zYpcVd.oPgzVFU5Rr3Rz8OA7113VfnzRttWhnmihN.akMZ54de64m
       }
-      http_port 7979
-      https_port 7979
       reverse_proxy http://127.0.0.1:3000
     '';
     virtualHosts."prometheus.jointhefreeworld.org".extraConfig = ''
       basic_auth {
         root $2a$14$zYpcVd.oPgzVFU5Rr3Rz8OA7113VfnzRttWhnmihN.akMZ54de64m
       }
-      http_port 7980
-      https_port 7980
       reverse_proxy http://127.0.0.1:9090
     '';
   };
