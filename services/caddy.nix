b33e40fc3c9be018595f7c5f1ff8373dec1ba2fe
Josep Bigorra <jjbigorra@gmail.com>
2024-09-07 15:51:12 +0200

    s

--- services/caddy.nix
+++ services/caddy.nix
@@ -2,6 +2,9 @@
   services.caddy = {
     enable = true;
     virtualHosts."grafana.jointhefreeworld.org".extraConfig = ''
+      basic_auth {
+        root 
+      }
       reverse_proxy http://127.0.0.1:3000
     '';
   };
