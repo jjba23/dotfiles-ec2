98cc6b049eb8af21aaa52dd10c82c06523c4f86d
Josep Bigorra <jjbigorra@gmail.com>
2024-09-07 16:35:22 +0200

    add prometheus port

--- services/caddy.nix
+++ services/caddy.nix
@@ -12,5 +12,11 @@
       }
       reverse_proxy http://127.0.0.1:3000
     '';
+    virtualHosts."prometheus.jointhefreeworld.org".extraConfig = ''
+      basic_auth {
+        root $2a$14$zYpcVd.oPgzVFU5Rr3Rz8OA7113VfnzRttWhnmihN.akMZ54de64m
+      }
+      reverse_proxy http://127.0.0.1:9090
+    '';
   };
 }
